d468603ed51cfd5b9f77906ca1bec4cdf625f607
Илиан Хороших <ilian10@mail.ru>
2024-02-22 02:11:14 +0300

    Main

--- hotel/core/urls.py
+++ hotel/core/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("galery/", views.gallery, name="gallery"),
+    path("contacts/", views.contacts, name="contacts"),
 
 ]
--- hotel/core/views.py
+++ hotel/core/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def gallery(request):
+    return render(request, 'core/gallery.html')
+
+def contacts(request):
+    return render(request, 'core/contact.html')
