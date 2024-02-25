d60d86f22b22868ce5fe393d13bbdf8b8c38082f
Илиан Хороших <ilian10@mail.ru>
2024-02-26 00:59:05 +0300

    add_features

--- hotel/hotel/settings.py
+++ hotel/hotel/settings.py
@@ -118,5 +118,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+CSRF_TRUSTED_ORIGINS = ['https://hotelanna.hopto.org']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
