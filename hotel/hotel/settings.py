7617a321ba63fc7ad84a0b4f8e97bfefa8d03cb7
Khoroshikh Ilian <81573935+Grandkol@users.noreply.github.com>
2024-07-12 20:48:26 +0300

    Try

--- hotel/hotel/settings.py
+++ hotel/hotel/settings.py
@@ -119,6 +119,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-CSRF_TRUSTED_ORIGINS = ["https://hotelanna.hopto.org"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
